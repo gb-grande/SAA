d7baa8798157d7b2f24ed13d1d1115d433474d82
lucas castro <lucas_o__castro@hotmail.com>
2024-04-21 12:52:47 -0300

    Removes papers from landing page and messes with responsiveness

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import {
     Paper, Title, Text,
-    Image, Space, Center, SimpleGrid, Flex
+    Image, Space, Center, SimpleGrid, Flex, Group
 } from "@mantine/core"
 import FlipCard from "../components/FlipCard.jsx";
 import PostCarousel from "../components/PostCarousel.jsx";
@@ -35,33 +35,29 @@ function LandingPage(){
                 </Flex>
             </Center>
 
-            <Paper m="md" shadow="xl" radius="md" withBorder p="xl" size="100%">
-                <SimpleGrid /*Seção quem somos - TO-DO: add image*/
-                    cols={{ base: 1, sm: 1, md: 2, lg: 2 }}
-                > 
-                    <div>
-                        <Title>Quem Somos</Title>
-                        <Space h="md" />
-                        <Text ta="justify" fz="lg">
-                            A Associação de Proteção aos Animais de Indaiatuba foi fundada em 1988, 
-                            com o objetivo de fiscalizar e coibir maus tratos contra animais, bem 
-                            como atuar no controle populacional  de cães e gatos conscientizando a 
-                            população sobre a importância da castração. A entidade busca atender ao 
-                            maior número possível de denúncias sobre maus tratos, além de orientar 
-                            tutores, atuar no controle de transmissão de zoonoses, ajudar famílias 
-                            carentes com consultas, vacinas e ração, entre outros serviços. A APRAI 
-                            tem o objetivo de ser a principal referência no apoio e defesa dos animais 
-                            do município de Indaiatuba.
-                        </Text>
-                    </div>
-                    <div>
-                        <Image
-                        radius="xl"
-                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
-                        />
-                    </div>
-                </SimpleGrid>
-            </Paper>
+            <SimpleGrid /*Seção quem somos - TO-DO: add image*/
+                cols={{ base: 1, sm: 1, md: 2, lg: 2 }}
+            >
+            <div>
+                    <Title>Quem Somos</Title>
+                    <Space h="md" />
+                    <Text ta="justify" fz="lg">
+                        A Associação de Proteção aos Animais de Indaiatuba foi fundada em 1988,
+                        com o objetivo de fiscalizar e coibir maus tratos contra animais, bem
+                        como atuar no controle populacional  de cães e gatos conscientizando a
+                        população sobre a importância da castração. A entidade busca atender ao
+                        maior número possível de denúncias sobre maus tratos, além de orientar
+                        tutores, atuar no controle de transmissão de zoonoses, ajudar famílias
+                        carentes com consultas, vacinas e ração, entre outros serviços. A APRAI
+                        tem o objetivo de ser a principal referência no apoio e defesa dos animais
+                        do município de Indaiatuba.
+                    </Text>
+                </div>
+                <Image
+                    radius="xl"
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                />
+            </SimpleGrid>
 
             <Paper h={{base: 400, sm: 500}} bg="aprai-purple.3" mt="md" mb="md" p="md" pb="xl" radius="xl">
                 <Title ta="center">Últimas Notícias</Title>
@@ -70,6 +66,16 @@ function LandingPage(){
             
             <Paper m="lg" shadow="xl" radius="md" withBorder p="xl" size="100%">
                 <Center>
+                <SimpleGrid /*Círculos informativos - TO-DO: add images*/
+                    cols={{ base: 1, sm: 2, lg: 3 }}
+                    spacing={{ base:'100px', sm: '100px', md: '125px', lg: '150px'}}
+                >
+
+                </SimpleGrid>
+                </Center>
+            </Paper>
+
+            <Center>
                 <SimpleGrid /*Círculos informativos - TO-DO: add images*/
                     cols={{ base: 1, sm: 2, lg: 3 }}
                     spacing={{ base:'100px', sm: '100px', md: '125px', lg: '150px'}}
@@ -88,8 +94,8 @@ function LandingPage(){
                     >
                         <Text>
                             <Image
-                            radius="xl"
-                            src=""
+                                radius="xl"
+                                src=""
                             />
                             <Text ta="center" size="50px" c="green" fw={700}>
                                 + 300
@@ -113,8 +119,8 @@ function LandingPage(){
                     >
                         <Text>
                             <Image
-                            radius="xl"
-                            src=""
+                                radius="xl"
+                                src=""
                             />
                             <Text ta="center" size="50px" c="green" fw={700}>
                                 + 100
@@ -138,8 +144,8 @@ function LandingPage(){
                     >
                         <Text>
                             <Image
-                            radius="xl"
-                            src=""
+                                radius="xl"
+                                src=""
                             />
                             <Text ta="center" size="50px" c="green" fw={700}>
                                 + 800
@@ -150,47 +156,45 @@ function LandingPage(){
                         </Text>
                     </div>
                 </SimpleGrid>
-                </Center>
-            </Paper>
+            </Center>
 
-            <Paper m="md" shadow="xl" radius="md" withBorder p="xl" size="100%">
-                <SimpleGrid /*Seção Contato - TO-DO: add links and map*/
-                    cols={{ base: 1, sm: 1, md: 2, lg: 2 }}
-                >
-                    <div>
-                        <Title>Contato</Title>
-                        <Space h="md" />
-                        <Text ta="center" size="xl" fw={500}>
-                            Endereço
-                        </Text>
-                        <Text ta="center" size="lg" fw={500}>
-                            Rua Onze de Junho, 684 - Centro, 
-                            Indaiatuba - SP, 13330-050
-                        </Text>
-                        <Space h="xl" />
-                        <Text ta="center" size="xl" fw={500}>
-                            Telefone
-                        </Text>
-                        <Text ta="center" size="lg" fw={500}>
-                            (19) 3835-7134
-                        </Text>
-                        <Space h="xl" />
-                        <Text ta="center" size="xl" fw={500}>
-                            Redes Sociais
-                        </Text>
-                        <Text ta="center" size="lg" fw={500}>
-                            Facebook: Aprai Indaiatuba <br />
-                            Instagram: @aprai.indaiatuba
-                        </Text>
-                    </div>
-                    <div>
-                        <Image
-                        radius="xl"
-                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
-                        />
-                    </div>
-                </SimpleGrid>
-            </Paper>
+            <SimpleGrid /*Seção Contato - TO-DO: add links and map*/
+                m="md"
+                cols={{ base: 1, sm: 1, md: 2, lg: 2 }}
+            >
+                <div>
+                    <Title ta="center">Contato</Title>
+                    <Space h="md" />
+                    <Text ta="center" size="xl" fw={500}>
+                        Endereço
+                    </Text>
+                    <Text ta="center" size="lg" fw={500}>
+                        Rua Onze de Junho, 684 - Centro,
+                        Indaiatuba - SP, 13330-050
+                    </Text>
+                    <Space h="xl" />
+                    <Text ta="center" size="xl" fw={500}>
+                        Telefone
+                    </Text>
+                    <Text ta="center" size="lg" fw={500}>
+                        (19) 3835-7134
+                    </Text>
+                    <Space h="xl" />
+                    <Text ta="center" size="xl" fw={500}>
+                        Redes Sociais
+                    </Text>
+                    <Text ta="center" size="lg" fw={500}>
+                        Facebook: Aprai Indaiatuba <br />
+                        Instagram: @aprai.indaiatuba
+                    </Text>
+                </div>
+                <div>
+                    <Image
+                    radius="xl"
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                    />
+                </div>
+            </SimpleGrid>
         </>
     );
 }
