6d7df62098354f36fe7bea56c50430407affdfe5
lucas castro <lucas_o__castro@hotmail.com>
2024-04-20 22:42:33 -0300

    Better FlipCard prop separation

--- frontend/src/components/FlipCard.jsx
+++ frontend/src/components/FlipCard.jsx
@@ -4,9 +4,9 @@ import {Card, Image, Text, Button, Group, Center, Stack} from "@mantine/core"
 
 // Exemplo:
 //
-//  <FlipCard 
-//      textFront="Nos ajude com a sua doação." 
-//      textBack="ABCD" 
+//  <FlipCard
+//      textFront="Nos ajude com a sua doação."
+//      textBack="ABCD"
 //      buttonText="DOE AGORA"
 //      image="https://media-gru2-1.cdn.whatsapp.net/v/t61.24694-24/424425093_286940224099553_19848131652106230_n.jpg?ccb=11-4&oh=01_Q5AaIHiORn1CH8ly8YGXOC7U2tbG67Z7gX4oYR6vMyvuIwHW&oe=6630039C&_nc_sid=e6ed6c&_nc_cat=103"
 //      imageAlt="Gato"/>
@@ -14,7 +14,7 @@ import {Card, Image, Text, Button, Group, Center, Stack} from "@mantine/core"
 //
 //
 
-function FlipCard(props) {
+function FlipCard({image, imageAlt, textFront, textBack, buttonText}) {
     const [isFlipped, setIsFliped] = useState(false);
 
     const handleClick = (e) => {
@@ -30,22 +30,22 @@ function FlipCard(props) {
                         <div>
                             <Card.Section>
                                 <Image
-                                    src={props.image}
-                                    alt={props.imageAlt}
+                                    src={image}
+                                    alt={imageAlt}
                                     h={200}
                                     />
                             </Card.Section>
                         </div>
                         <Stack align='center' gap='lg'>
-                            <Text>{props.textFront}</Text> 
-                            <Button onClick={handleClick} radius="lg">{props.buttonText}</Button>
+                            <Text>{textFront}</Text>
+                            <Button onClick={handleClick} radius="lg">{buttonText}</Button>
                         </Stack>
                     </Group>
                 </Card>
                 
                 <Card h={200} radius="md" withBorder>
                     <Stack h={200} align='center' justify='space-around'>
-                        <Text>{props.textBack}</Text>
+                        <Text>{textBack}</Text>
                         <Button onClick={handleClick} radius="lg">Voltar</Button>
                     </Stack>
                 </Card>
