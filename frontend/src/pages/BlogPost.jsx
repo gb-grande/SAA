559ea6ab72e90f0274ecac1210543b5d97709cd3
LelioMarcos <leliomrcunha@usp.br>
2024-04-25 17:22:41 -0300

    feat: blog post page

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -1,6 +1,7 @@
 import {
     Paper, Title, Text,
-    Image, Space, Center, SimpleGrid
+    Image, Space, Center, SimpleGrid,
+    Group
 } from "@mantine/core"
 import { Link, useParams } from 'react-router-dom';
 
@@ -8,27 +9,34 @@ import { Link, useParams } from 'react-router-dom';
 
 function BlogPost() {
     const {id} = useParams();
+    const today = new Date();
     const post = {
         title: "APRAI",
-        time: "",
-        image: "https://media-gru2-1.cdn.whatsapp.net/v/t61.24694-24/424425093_286940224099553_19848131652106230_n.jpg?ccb=11-4&oh=01_Q5AaIHiORn1CH8ly8YGXOC7U2tbG67Z7gX4oYR6vMyvuIwHW&oe=6630039C&_nc_sid=e6ed6c&_nc_cat=103",
+        time: today,
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEitxnXh3BCHXlUOIPJdO1y51fEPdGCGf9bImEnIUVo_uPy5ECws2w1iAI2wrDs8Sc0oVbtdnYZvFG5UzykV-qvcWamD-Wyj6MOvh6UCTXHPH-5Xf-B5tH-8BqYSvw4roWyud7AgdE_3eXc/s800/pet_omocha_inu.png",
         text: `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel tincidunt purus, vel vulputate augue. Integer ut ex metus. Nulla imperdiet lobortis felis, sed pellentesque magna rutrum aliquet. Maecenas nec tincidunt leo, eu faucibus lectus. Maecenas hendrerit purus et diam rhoncus scelerisque. Cras tempor odio ac mi sodales, non laoreet nibh egestas. Quisque non luctus lacus.\n
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel tincidunt purus, vel vulputate augue. Integer ut ex metus. Nulla imperdiet lobortis felis, sed pellentesque magna rutrum aliquet. Maecenas nec tincidunt leo, eu faucibus lectus. Maecenas hendrerit purus et diam rhoncus scelerisque. Cras tempor odio ac mi sodales, non laoreet nibh egestas. Quisque non luctus lacus. 
+        Quisque pulvinar faucibus elementum. Ut cursus augue vitae consectetur tincidunt. Pellentesque dignissim, diam in ullamcorper rhoncus, ex tellus pharetra lorem, sed accumsan nisl dui consequat ipsum. Donec mollis vitae tortor faucibus consectetur. Aenean dolor urna, dapibus ut risus ut, aliquet tempor nunc. Suspendisse tempor dignissim nunc id mattis. Nullam at magna lorem.
         
-        Quisque pulvinar faucibus elementum. Ut cursus augue vitae consectetur tincidunt. Pellentesque dignissim, diam in ullamcorper rhoncus, ex tellus pharetra lorem, sed accumsan nisl dui consequat ipsum. Donec mollis vitae tortor faucibus consectetur. Aenean dolor urna, dapibus ut risus ut, aliquet tempor nunc. Suspendisse tempor dignissim nunc id mattis. Nullam at magna lorem.\n
-        
-        Fusce id magna nec purus accumsan fringilla. Sed porta in velit ac pharetra. Mauris condimentum sapien in urna gravida accumsan. Donec porta rhoncus arcu. Nullam convallis sem mauris, eu malesuada est ornare venenatis. Curabitur nec consectetur diam, eu ultrices ipsum. Sed viverra faucibus sem, non scelerisque nulla congue varius. Pellentesque quam quam, porttitor quis nulla vitae, consequat blandit lacus. Integer id nisi nec orci cursus condimentum molestie ac metus.\n
-        
-        Duis luctus accumsan lacus, ut suscipit lectus fermentum a. Maecenas nulla turpis, rhoncus sed mi vitae, mattis egestas ipsum. Sed auctor odio risus, sed luctus mauris bibendum vel. Aenean leo tellus, aliquet et finibus pretium, blandit in est. Donec congue urna nibh. Vestibulum pellentesque est quis eleifend facilisis. Sed blandit ligula et quam dignissim, ac sagittis turpis vulputate.\n
-        
-        In id vulputate sapien. Quisque vel nisl posuere metus imperdiet venenatis. Morbi gravida, dui sollicitudin finibus hendrerit, eros nisi volutpat nisi, id condimentum diam dui ac massa. Nulla vel urna urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elementum sit amet dui in sodales. Nullam a elit sed sem euismod maximus.\n
+        Fusce id magna nec purus accumsan fringilla. Sed porta in velit ac pharetra. Mauris condimentum sapien in urna gravida accumsan. Donec porta rhoncus arcu. Nullam convallis sem mauris, eu malesuada est ornare venenatis. Curabitur nec consectetur diam, eu ultrices ipsum. Sed viverra faucibus sem, non scelerisque nulla congue varius. Pellentesque quam quam, porttitor quis nulla vitae, consequat blandit lacus. Integer id nisi nec orci cursus condimentum molestie ac metus.
+        Duis luctus accumsan lacus, ut suscipit lectus fermentum a. Maecenas nulla turpis, rhoncus sed mi vitae, mattis egestas ipsum. Sed auctor odio risus, sed luctus mauris bibendum vel. Aenean leo tellus, aliquet et finibus pretium, blandit in est. Donec congue urna nibh. Vestibulum pellentesque est quis eleifend facilisis. Sed blandit ligula et quam dignissim, ac sagittis turpis vulputate.
+        In id vulputate sapien. Quisque vel nisl posuere metus imperdiet venenatis. Morbi gravida, dui sollicitudin finibus hendrerit, eros nisi volutpat nisi, id condimentum diam dui ac massa. Nulla vel urna urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elementum sit amet dui in sodales. Nullam a elit sed sem euismod maximus.
     `
     };
-
+    
     return (
         <>
             <Title>{post.title}</Title>
-            <Text>{post.text}</Text>
+            <Text c="aprai-purple.9">Postado em {post.time.getDate()}/{post.time.getMonth() + 1}/{post.time.getFullYear()}, as {post.time.getHours()}:{post.time.getMinutes()}</Text>
+            <Center>
+                <Image m="md" w={{lg: 350, md:300, sm: 250, base: 200}} radius="xl" src={post.image} />
+            </Center>
+            <Text 
+                ml={{base: "5%", sm: "10%"}} 
+                mr={{base: "5%", sm: "10%"}} 
+                style={{whiteSpace: "pre-line", textAlign: "justify"}}>
+                    {post.text}
+            </Text>
         </>
     )
 }
