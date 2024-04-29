c31c07dc20686dc3157892a3cf8352ff6fa56d52
Ensuo <63194069+Ensuo@users.noreply.github.com>
2024-04-29 10:46:07 -0300

    Added the base for footer and header design

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,8 +1,10 @@
 import {useDisclosure} from "@mantine/hooks";
-import {AppShell, Burger, Group, Anchor, Center, Stack, Image} from "@mantine/core";
+import {AppShell, Burger, Group, Anchor, Center, Stack, Image, Text, ActionIcon} from "@mantine/core";
 import {Outlet} from "react-router-dom";
+import { IconBrandFacebook, IconBrandInstagram } from "@tabler/icons-react";
 import logo from '../assets/logo.jpeg';
 import App from "../App";
+
 
 function Layout() {
     const [opened, {toggle}] = useDisclosure();
@@ -20,11 +22,15 @@ function Layout() {
 
     let headerHeight = 60;
     if (opened) headerHeight += 30 * links.length;
+    
+    let footerHeight = 90;
 
     return (
         <AppShell
             header={{ height: headerHeight }}
             padding="md"
+
+            footer={{height: footerHeight}}
         >   
             <AppShell.Header bg={'#392F88'}>
                 <Center h={'100%'} px="md">
@@ -38,12 +44,27 @@ function Layout() {
                 </Center>
             </AppShell.Header>
 
-                <AppShell.Main>
-                    <Outlet/>
-                </AppShell.Main>
+            <AppShell.Main>
+                <Outlet/>
+            </AppShell.Main>
 
-            <AppShell.Footer>
-                
+            <AppShell.Footer bg={'#392F88'} ff={'Karla'} c={'white'} mt="auto">
+              
+                <Group justify="space-between" p="md">
+                    <Stack gap="0">
+                        <Text fz="h2">APRAI</Text>
+                        <Text fz="h4">Associação de Proteção aos Animais de Indaiatuba</Text>
+                    </Stack>
+                    <ActionIcon.Group>
+                        <ActionIcon variant="subtle" color="white" size="xl">
+                            <IconBrandInstagram style={{width: '100%', height: '100%'}}></IconBrandInstagram>
+                        </ActionIcon>
+                        <ActionIcon variant="subtle" color="white" size="xl">
+                            <IconBrandFacebook style={{width: '100%', height: '100%'}}></IconBrandFacebook>
+                        </ActionIcon>
+                    </ActionIcon.Group>
+                </Group>
+             
             </AppShell.Footer>
         </AppShell>
     );
