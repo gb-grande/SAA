4ea6131942967f7fc0940410f9ee17d71ac0948a
lucas castro <lucas_o__castro@hotmail.com>
2024-05-02 23:57:11 -0300

    Replaces broken flipcard image

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -21,7 +21,7 @@ function LandingPage(){
                     textFront="Nos ajude com a sua doação."
                     textBack="Informações da Doação"
                     buttonText="DOE AGORA"
-                    image="https://media-gru2-1.cdn.whatsapp.net/v/t61.24694-24/424425093_286940224099553_19848131652106230_n.jpg?ccb=11-4&oh=01_Q5AaIHiORn1CH8ly8YGXOC7U2tbG67Z7gX4oYR6vMyvuIwHW&oe=6630039C&_nc_sid=e6ed6c&_nc_cat=103"
+                    image="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     imageAlt="Gato (Siri)"/>
 
                 <FlipCard
