c93f2f6068cce6eb17006ebde28f9d0e24e406dc
lucas castro <lucas_o__castro@hotmail.com>
2024-05-12 21:02:12 -0300

    Sets up axios url in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import {MantineProvider} from "@mantine/core";
+import {ModalsProvider} from "@mantine/modals";
 import theme from "./theme.js";
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/tiptap/styles.css';
 import './index.css'
-import {ModalsProvider} from "@mantine/modals";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
