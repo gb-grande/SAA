a55e17cb3d2278e4b59df951ec2897e6b5f44e2d
lucas castro <lucas_o__castro@hotmail.com>
2024-05-24 10:53:58 -0300

    Forgot to rename error in EditableSectionText.jsx

--- frontend/src/components/EditableSectionText.jsx
+++ frontend/src/components/EditableSectionText.jsx
@@ -3,20 +3,24 @@ import axios from "axios";
 import useFetch from "../hooks/useFetch.jsx";
 
 function EditableSectionText({section, containerStyle, inputStyle, textClassName, maxLen}){
-    const {result, setResult, error} = useFetch(`api/infoTexts/${section}`, null, '');
+    const {result: text, setResult: setText, error} = useFetch(`api/infoTexts/${section}`, null, '');
     if (error){
-        console.error(`Couldn't load '${section}' info.`, textError);
+        console.error(`Couldn't load '${section}' info.`, error);
     }
 
     function onSave(value){
         axios.post(`api/infoTexts/${section}`, {data: value})
-            .then(_ => console.log(`Updated ${section} text.`))
+            .then(_ => {
+                console.log(`Updated ${section} text.`);
+                setText(value);
+            })
             .catch(err => console.error(`Failed to update ${section} text.`, err));
-        setResult(value);
     }
 
     return (
-        <EditableText inputStyle={inputStyle} containerStyle={containerStyle} textClassName={textClassName} text={result} onSave={onSave} maxLen={maxLen}/>
+        <EditableText inputStyle={inputStyle} containerStyle={containerStyle} textClassName={textClassName}
+                      text={text} onSave={onSave} maxLen={maxLen}
+        />
     )
 }
 
