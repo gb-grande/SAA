b40333adf13e821b8008bb49848ed4bf34d671a6
Albert <albshoji@gmail.com>
2024-05-26 17:43:33 -0300

    connecting to db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import http from 'http'
 import cors from 'cors'
 import helmet from "helmet";
@@ -17,6 +18,19 @@ app.use('/api/contactInfos', contactInfos)
 const server = http.createServer(app)
 
 const port = process.env.PORT || 4000
+
+const uri = ""
+
+async function connect(){
+    try{
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.error(error);
+    }
+}
+
+connect();
 
 server.listen(port, () => {
     console.log(`Server running in port ${port}`);
