d7329628a8fb93ed2bc68efcc480de295f27b503
lucas castro <lucas_o__castro@hotmail.com>
2024-05-28 02:02:33 -0300

    Hashes password before storing in db

--- backend/src/controllers/admins.js
+++ backend/src/controllers/admins.js
@@ -1,10 +1,14 @@
 import Admin from "../models/Admin.js";
+import bcrypt from "bcrypt"
+import '../config.js'
 
 export async function registerAdmin(req, res){
     try {
+        const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
         const admin = new Admin({
             user: req.body.user,
-            password: req.body.password
+            password: hashedPassword
         });
         await admin.save();
         return res.status(201).send('Admin registered.');
@@ -12,6 +16,7 @@ export async function registerAdmin(req, res){
         if (e.code === 11000){
             return res.status(409).send('Duplicate user.');
         }
+        console.error('Unhandled error registering admin.', e);
         return res.status(400).send('Error registering admin.')
     }
 }
