4f99300a052a6a29fbae092fd61dd3e51cee47f5
lucas castro <lucas_o__castro@hotmail.com>
2024-05-29 20:12:15 -0300

    Post creation route and controller

--- backend/src/controllers/posts.js
+++ backend/src/controllers/posts.js
@@ -1,2 +1,26 @@
 import Post from "../models/Post.js";
 import mongoose from "mongoose";
+
+export async function createPost(req, res) {
+    try {
+        //TODO upload image
+        const blog = new Post({
+            posterUsername: req.body.posterUsername,
+            date: req.body.date ?? Date.now(),
+            title: req.body.title,
+            imageId: req.body.imageId,
+            content: req.body.content
+        });
+        const saved = await blog.save();
+        return res.status(201).send({id: saved._id});
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError){
+            const errors = Object.values(e.errors).map(e => {
+                return {path: e.path, type: e.kind}
+            });
+            return res.status(400).send(errors);
+        }
+        console.log('Unhandled error in blog creation.', e);
+        return res.status(500).send('Error during blog creation.');
+    }
+}
--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,5 +1,8 @@
 import express from "express";
+import {createPost} from "../controllers/posts.js";
+
 const router = express.Router();
 
+router.post('/', createPost);
 
 export default router;
