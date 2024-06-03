958be17d6badf0b90e5be15e2dcbb8958b8fa544
lucas castro <lucas_o__castro@hotmail.com>
2024-06-03 18:59:12 -0300

    Installs mantine notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import {BrowserRouter} from "react-router-dom";
 import {MantineProvider} from "@mantine/core";
 import {ModalsProvider} from "@mantine/modals";
@@ -8,13 +8,16 @@ import theme from "./theme.js";
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/tiptap/styles.css';
-import './index.css'
+import '@mantine/notifications/styles.css';
+import './index.css';
+import {Notifications} from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
           <MantineProvider theme={theme}>
               <ModalsProvider>
+                  <Notifications/>
                   <App/>
               </ModalsProvider>
           </MantineProvider>
