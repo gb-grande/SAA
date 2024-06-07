017210367899bae5ff2f8d59d78d092350ddda64
lucas castro <lucas_o__castro@hotmail.com>
2024-06-07 01:43:51 -0300

    Fixes date related error

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -27,7 +27,7 @@ export function PostCard({post, h, w, light=false, showDate=true, imgHPct=0.6, .
 
             <Card.Section p='sm' pt={imageUrl ? 0 : 'sm'}>
                 {showDate &&
-                    <Text size='xs' c={textColor}>{post.date.split('T')[0]}</Text>
+                    <Text size='xs' c={textColor}>{post.date?.toLocaleDateString()}</Text>
                 }
                 <Title order={4} lineClamp={2} c={textColor}>{title}</Title>
                 <Text c={textColor} lineClamp={lineCount} dangerouslySetInnerHTML={{__html: content}}/>
