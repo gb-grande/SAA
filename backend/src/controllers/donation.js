7edb640809078511ee3abaa632d81bdb348469f5
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2024-06-17 21:11:31 -0300

    get single donation and create donation route done

--- backend/src/controllers/donation.js
+++ backend/src/controllers/donation.js
@@ -2,6 +2,21 @@ import Donation from "../models/Donation";
 import mongoose from "mongoose";
 
 
+export async function registerDonation(req, res) {
+    try{
+        const newDonation = new Donation(req.body);
+        const savedDonation = await newDonation.save();
+        return res.status(201).send({id: savedDonation._id});
+    } catch(e){
+        if (e instanceof mongoose.Error.ValidationError){
+            const errors = Object.values(e.errors).map(e => ({[e.path]: e.message}));
+            return res.status(400).send({validationErrors: Object.assign({}, ...errors)});
+        }
+        console.error('Unhandled error registering donation.', e);
+        return res.status(500).send({message: 'Error ao registrar doação.'})
+    }
+}
+
 export async function getDonations(req, res) {
     try {
         const donations = await Donation.find(null, null, {sort: {date: -1}});
@@ -10,6 +25,20 @@ export async function getDonations(req, res) {
         return res.status(400).send({ error: e.message });
     }
 }
+
+
+export async function getDonation(req, res) {
+    try {
+
+        const donation = await Donation.findById(req.params.id);
+        return (donation) 
+            ? res.status(200).send(donation)
+            : res.status(404).send({message: 'Doação não existe'});
+    } catch (e){
+        return res.status(400).send({ error: e.message });
+    }
+}
+
 
 export async function deleteDonation(req, res) {
     try {
