c2334426a424f9488d401535a412bc2a53f212f8
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2024-06-18 09:37:50 -0300

    fixed donations model

---
+++ backend/src/models/Donations.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const donationsSchema = new mongoose.Schema({
+    admin_username: {
+        type: String,
+        required: [true, 'O administrador que preencheu é obrigatório.'],
+    },
+    source: {
+        type: String,
+        required: [true, 'A origem da doação é obrigatória.']
+    }, 
+    destination: {
+        type : String
+    },
+    type: {
+        type: String,
+        required: [true, 'O tipo é obrigatório']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'A quantidade é obrigatória']
+    }
+});
+
+const Donations = new mongoose.model("Donations", donationsSchema);
+export default Donations;
