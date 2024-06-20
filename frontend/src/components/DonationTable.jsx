e17bb7152131a3573209dfb2102c299d31ae7e9d
Albert <albshoji@gmail.com>
2024-06-20 14:59:38 -0300

    implementing frontend donations

--- frontend/src/components/DonationTable.jsx
+++ frontend/src/components/DonationTable.jsx
@@ -4,20 +4,21 @@ import { Table } from '@mantine/core';
 
 function DonationTable() {
   const elements = [
-    { type: "tusca", amount: 12.011, source: 'C', destination: 'Carbon' },
-    { type: "tusca", amount: 14.007, source: 'N', destination: 'Nitrogen' },
-    { type: "tusca", amount: 88.906, source: 'Y', destination: 'Yttrium' },
-    { type: "tusca", amount: 137.33, source: 'Ba', destination: 'Barium' },
-    { type: "tusca", amount: 140.12, source: 'Ce', destination: 'Cerium' },
+    { type: "tusca", amount: 12.011, source: 'C', destination: 'Carbon', date: "12/12/1222"},
+    { type: "tusca", amount: 14.007, source: 'N', destination: 'Nitrogen', date: "12/12/1222"},
+    { type: "tusca", amount: 88.906, source: 'Y', destination: 'Yttrium', date: "12/12/1222"},
+    { type: "tusca", amount: 137.33, source: 'Ba', destination: 'Barium', date: "12/12/1222"},
+    { type: "tusca", amount: 140.12, source: 'Ce', destination: 'Cerium', date: "12/12/1222"},
   ];
 
 
     const rows = elements.map((element) => (
       <Table.Tr key={element.name}>
         <Table.Td>{element.type}</Table.Td>
+        <Table.Td>{element.amount}</Table.Td>
         <Table.Td>{element.destination}</Table.Td>
         <Table.Td>{element.source}</Table.Td>
-        <Table.Td>{element.amount}</Table.Td>
+        <Table.Td>{element.date}</Table.Td>
       </Table.Tr>
     ));
   
@@ -29,6 +30,7 @@ function DonationTable() {
             <Table.Th>Quantidade</Table.Th>
             <Table.Th>Origem</Table.Th>
             <Table.Th>Destino</Table.Th>
+            <Table.Th>Data</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
