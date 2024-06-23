6ea9097d5a619df672b81a12439f395966e9b158
Gustavo Grande <gustavobgrande@hotmail.com>
2024-06-23 17:54:40 -0300

    working pdf gen but unalligned

--- backend/src/services/donationReportGen.js
+++ backend/src/services/donationReportGen.js
@@ -12,7 +12,7 @@ const items = [
     },
     {
         srcDest: "Porto Alegre",
-        type: "cachorro",
+        type: "gatoaff",
         amount: 3,
         date: new Date('2024-06-21'),
         flow: "sent"
@@ -26,7 +26,7 @@ const items = [
     },
     {
         srcDest: "Brasília",
-        type: "cachorro",
+        type: "cacho",
         amount: 4,
         date: new Date('2024-06-23'),
         flow: "sent"
@@ -35,7 +35,7 @@ const items = [
         srcDest: "Fortaleza",
         type: "gato",
         amount: 1,
-        data: new Date('2024-06-24'),
+        date: new Date('2024-06-24'),
         flow: "received"
     }
 ];
@@ -65,7 +65,7 @@ const donationPlaceholder = new DonationsInfo(items);
 function buildEmptyReport(startDate, endDate){
     const doc = new PDFDocument({size: 'A4'});
     //add header
-    doc.fontSize(16);
+    doc.fontSize(22);
     doc.text("RELATÓRIO DE DOAÇÕES - APRAI", {
         align: "center"
     })
@@ -74,42 +74,63 @@ function buildEmptyReport(startDate, endDate){
     const today = new Date(Date.now());
     doc.fontSize(10);
     doc.text(`Gerado em: ${("0" + today.getDate()).slice(-2)}/${("0" + today.getMonth()).slice(-2) }/${1900 + today.getYear()}`);
+    doc.moveDown(2);
     //add start and end date
-    doc.fontSize(12);
-    doc.text(`Data inicial: ${startDate}`, {
-        align: "left"
+    doc.fontSize(14);
+    doc.text(`Data inicial: ${startDate}      Data final: ${endDate}`, {
+        align: "left",
     });
-    doc.text(`Data final:   ${endDate}`, {
-        align: "left"
-    });
+    doc.moveDown();
     return doc;
 }
 
 function insertSummary(doc, donationsInfo){
-    doc.text(`Total recebido: ${donationsInfo.totalReceived}kgs`);
-    doc.text(`Total enviado: ${donationsInfo.totalSent}kgs`);
+    doc.fontSize(14);
+    doc.text(`Total recebido: ${donationsInfo.totalReceived.toFixed(1)}kgs`);
+    doc.text(`Total enviado: ${donationsInfo.totalSent.toFixed(1)}kgs`);
+    doc.text(`Variação do estoque: ${(donationsInfo.totalReceived - donationsInfo.totalSent).toFixed(1)}kgs`)
 }
 
-// function insertTableHeader(doc){
-//     doc.moveTo(doc.x, doc.y).lineTo(doc.x + 100, doc.y);
-// }
+ function insertSectionHeader(doc, header){
+    doc.moveTo(0, doc.y).lineTo(595.28, doc.y).stroke();
+    doc.moveDown(2);
+    doc.fontSize(18)
+    doc.text(`${header}`, {
+        align: "center"
+    });
+    doc.moveDown();
+ }
+
+function insertTableHeaders(doc, isReceived){
+    doc.fontSize(14);
+    let outputString = `Data                 Tipo                            Quantidade  ${isReceived ? "Origem" : "Destino"}`;
+    doc.font("Helvetica-Bold");
+    doc.text(outputString, {
+        width: 600,
+        align: "left"
+    })
+    doc.font("Helvetica");
+
+
+
+}
 
 //Inserts row of data into table
 function insertRow(doc, entry){
-    let {srcDest, type, amount, date, flow} = entry
-    //Thy Date of different sizes. Thus, they must be formatted so that the row is correctly sized
-   doc.text("textoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n", {
-        lineGap: 5,
-        width: 100,
-        align: 'center'
-     })
-      
-}
-
-function genTable(doc, data){
-    data.forEach((entry) => {
-        insertRow(doc, data)
+    doc.fontSize(14);
+    let {srcDest, type, amount, date} = entry
+    //adding space so it can be properly printed aligned
+    const dateString = ( "0" + date.getDate()).slice(-2) + "/" + ( "0" + date.getMonth()).slice(-2) + "/" + String((1900 + date.getYear()));
+    const dateWithSpace = dateString + " ".repeat(7);
+    const typeWithSpace = (type + " ".repeat(40)).slice(0, 32);
+    const amountWithSpace = (amount.toFixed(2) + " ".repeat(16)).slice(0, 18);
+    const outputString = dateWithSpace + typeWithSpace + amountWithSpace + srcDest;
+    //srcDest doesn't need padding because it's the last one
+    console.log(outputString);
+    doc.text(outputString, {
+        align: "left"
     })
+
 }
 
 
@@ -122,10 +143,34 @@ function endDoc(doc){
 }
 
 
+//function that generates final pdf
+function generatePdf(startDateString, endDateString, allDonations){
+    const donationInfo = new DonationsInfo(allDonations);
+    let report = buildEmptyReport(startDateString, endDateString);
+    insertSectionHeader(report, "Métricas Gerais")
+    insertSummary(report, donationPlaceholder);
+    report.moveDown(1);
+    //received section
+    insertSectionHeader(report, "Recebidos")
+    insertTableHeaders(report, true);
+    report.moveDown();
+    //insert all received
+    for (let r of donationInfo.donationIn) {
+        insertRow(report, r);
+    }
+    report.moveDown();
+    //sent section
+    insertSectionHeader(report, "Enviados")
+    insertTableHeaders(report, false);
+    report.moveDown();
+    //insert all received
+    for (let s of donationInfo.donationOut) {
+        insertRow(report, s);
+    }
+    endDoc(report)
 
-let report = buildEmptyReport("01/01/2001", "05/09/2023");
-insertSummary(report, donationPlaceholder);
-//insertTableHeader(report)
-insertRow(report, items[0])
-insertRow(report, items[0])
-endDoc(report)
+
+
+}
+
+generatePdf("01/01/2001", "05/09/2023", items);
