14642ed4a57b2c9e2f1c00e1e1b272b7cb22f087
MiguelHenri <miguelbrahen@gmail.com>
2024-06-23 23:17:05 -0300

    documenting backend models

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,5 +1,12 @@
 import jwt from 'jsonwebtoken'
 
+/**
+ * Middleware function to authenticate incoming requests.
+ * 
+ * @param {object} req - Request object, JWT token provided in header.
+ * @param {object} res - Response object.
+ * @param {function} next - Callback function to pass control to the next middleware.
+ */
 async function authMidd(req, res, next) {
     const tokenHead = req.header('Authorization');
     const token = tokenHead && tokenHead.split(' ')[1];
